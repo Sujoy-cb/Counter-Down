d76913edbdbe0d2a88b6cbfa97ee81055a8f4059
Sujoy-cb <sujoyc070@gmail.com>
2021-01-07 23:27:22 +0600

    change in h4

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 $(document).ready(function(){
     loopcounter('counter-class');
 });
+
+
+
+
+/*----------sticky bar---------*/
+
+$(window).scroll(function(){
+    var scrollamount = $(window).scrollBottom()
+    if(scrollamount > 50){
+      $("#Timer-part").addClass("fixed")
+    }else{
+      $("#Timer-part").removeClass("fixed")
+    }
+  })
